c841c11509c9bf17acf464aa09ee16338690d7c1
Lysdora <lysdora.web@gmail.com>
2023-07-14 12:14:42 +0200

    add TODO list

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+TODO: /* 
+Create player
+Shoot projectiles
+Create enemies
+Detect collision on enemy / projectile hit
+Detect collision on enemy / player hit
+Remove off screen projectiles
+Colorize game
+Shrink enemies on hit
+Create particle explosion on hit
+Add score
+Add game overUI
+Add restart button
+Add start game button
+*/
+
+
 const canvas = document.querySelector("canvas");
 
 canvas.width = innerWidth; // window.innerWidth is the same
