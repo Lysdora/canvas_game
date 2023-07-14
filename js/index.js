d1819e6cb9574e4a2a526eb348cc273059186bd2
Lysdora <lysdora.web@gmail.com>
2023-07-14 12:09:11 +0200

    add css file and const canvas

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector("canvas");
+
+canvas.width = innerWidth; // window.innerWidth is the same
+canvas.height = innerHeight; // window.innerHeight
+console.log(canvas);
